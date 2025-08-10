df5c237ac06d4edd26c2dbd339d70befccbaf418
ryxxng <110778273+LeeYeRyeong@users.noreply.github.com>
2025-08-10 22:45:26 +0900

    baekjoon 34073

---
+++ baekjoon34073.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> s;
+        cout << s << "DORO ";
+    }
+    
+    return 0;
+}
